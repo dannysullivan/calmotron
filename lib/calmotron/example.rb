45c4c3d9e4b7bb61e927e5bfe3bd7dfcc9366636
Daniel Sullivan <danielalexandersullivan@gmail.com>
2015-04-29 10:42:29 -0400

    Create example usage script

--- lib/calmotron/example.rb
+++ lib/calmotron/example.rb
@@ -5,17 +5,15 @@ require_relative 'song_section'
 require_relative 'melody_builder'
 require_relative 'music_theory'
 require 'midilib'
-require 'pry'
 
-song = Song.new('B')
+song = Song.new('D')
 song.add_track(1, 4)
 song.add_track(3, 2)
 song.add_track(4, 1)
 
-song.add_section((1..5).to_a)
-song.add_section((1..5).to_a)
-song.add_section((1..5).to_a)
-song.add_section((1..5).to_a)
+song.add_section(MusicTheory::PENTATONIC_SCALE)
+song.add_section(MusicTheory::MAJOR_SCALE)
+song.add_section(MusicTheory::PENTATONIC_SCALE)
 
 output = song.to_midi_sequence
 open('asdf.mid', 'w') { |file| output.write(file) }
--- lib/calmotron/music_theory.rb
+++ lib/calmotron/music_theory.rb
@@ -16,6 +16,9 @@ module MusicTheory
   }
 
   PENTATONIC_SCALE = [0, 2, 4, 7, 9]
+  MAJOR_SCALE = [0, 2, 4, 5, 7, 9, 11]
+  MINOR_SCALE = [0, 2, 3, 5, 6, 8, 10]
+  QUARTAL_SCALE = [-5, 0, 5, 10, 15]
 
   def self.semitones_from_tonic_to_midi_note(semitones_from_tonic, octave, key)
     FIRST_NOTE_IN_KEY[key] + (12 * octave) + semitones_from_tonic
